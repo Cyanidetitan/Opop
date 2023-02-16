ab78d1f5cfdc3b77f4a9e8829c6ed8b222404252
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-16 23:45:48 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,9 +6,9 @@ from decouple import config
 try:
     APP_ID = "10247139"
     API_HASH = "96b46175824223a33737657ab943fd6a"
-    BOT_TOKEN = "5210009358:AAESvuzGgAhRITt0BZxgrMjnRqlq2yDf18Q"
+    BOT_TOKEN = "5912998821:AAHnfwQMIgzQ5GgaAw1iyiDIts11pn2SKkY"
     DEV = 1664850827
-    OWNER = "1443454117"
+    OWNER = "5371981851"
     ffmpegcode = ["-preset medium -c:v h265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1 -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 8"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
 except Exception as e:
